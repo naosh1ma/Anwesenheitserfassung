c45da22f9bff26b43f01a083ea438d8c0aedfb55
Arthur Adigamov <grinshif@gmail.com>
2025-04-01 11:52:16 +0200

    -

--- src/main/java/com/art/erfassung/controller/ErfassungController.java
+++ src/main/java/com/art/erfassung/controller/ErfassungController.java
@@ -42,38 +42,12 @@ public class ErfassungController {
         if (gruppe == null) {
             return "gruppen"; // Falls die Gruppe nicht existiert
         }
-
         List<Studenten> studentenListe = studentenRepository.findByGruppeId(gruppe.getId());
         List<Status> statusListe = statusRepository.findAll(); // Status-Liste für Dropdown
-
         model.addAttribute("studentenListe", studentenListe);
         model.addAttribute("statusListe", statusListe);
         model.addAttribute("gruppe", gruppe);
-
         return "anwesenheit";
-    }
-
-
-    @GetMapping("/liste/{gruppenId}")
-    public String anwesenheitAnzeigen(@PathVariable Integer gruppenId,
-                                      @RequestParam(required = false) String monat,
-                                      Model model) {
-
-        Gruppe gruppe = gruppeRepository.findById(gruppenId).orElse(null);
-        List<Studenten> studenten = studentenRepository.findByGruppeId(gruppe.getId());
-
-        LocalDate monatStart = (monat != null) ? LocalDate.parse(monat + "-01") : LocalDate.now().withDayOfMonth(1);
-        LocalDate monatEnde = monatStart.withDayOfMonth(monatStart.lengthOfMonth());
-
-        List<Erfassung> erfassungen = erfassungRepository.findByStudenten_GruppeIdAndDatumBetween(gruppe.getId(), monatStart, monatEnde);
-
-        model.addAttribute("gruppe", gruppe);
-        model.addAttribute("studenten", studenten);
-        model.addAttribute("erfassungen", erfassungen);
-        model.addAttribute("monat", monatStart.toString().substring(0, 7)); // YYYY-MM
-        model.addAttribute("tageImMonat", monatStart.lengthOfMonth());
-
-        return "anwesenheitsliste";
     }
 
     @PostMapping("/speichern")
@@ -82,38 +56,51 @@ public class ErfassungController {
                                        @RequestParam("kommentar") List<String> kommentare) {
 
         LocalDate datum = LocalDate.now();
-
         for (int i = 0; i < studentenIds.size(); i++) {
             Studenten student = studentenRepository.findById(studentenIds.get(i)).orElse(null);
             Status status = statusRepository.findById(statusIds.get(i)).orElse(null);
             String kommentar = kommentare.get(i);
-
             if (student != null && status != null) {
                 Erfassung erfassung = new Erfassung(student, datum, status, kommentar);
                 erfassungRepository.save(erfassung);
             }
-
         }
         return "redirect:/anwesenheit/" + studentenIds.get(0); // Zurück zur Gruppe
     }
 
-
-    @PostMapping("/liste/update/{id}")
-    public String updateAnwesenheit(
-            @PathVariable Integer id,
-            @RequestParam Integer statusId,
-            @RequestParam String kommentar) {
-
-        Optional<Erfassung> optionalErfassung = erfassungRepository.findById(id);
-        if (optionalErfassung.isPresent()) {
-            Erfassung erfassung = optionalErfassung.get();
-            Status neuerStatus = statusRepository.findById(statusId).orElse(null);
-            if (neuerStatus != null) {
-                erfassung.setStatus(neuerStatus);
-                erfassung.setKommentar(kommentar);
-                erfassungRepository.save(erfassung);
-            }
-        }
-        return "redirect:/anwesenheitsliste/" + optionalErfassung.get().getStudenten().getGruppe().getId();
-    }
+//    @GetMapping("/liste/{gruppenId}")
+//    public String anwesenheitAnzeigen(@PathVariable Integer gruppenId,
+//                                      @RequestParam(required = false) String monat,
+//                                      Model model) {
+//        Gruppe gruppe = gruppeRepository.findById(gruppenId).orElse(null);
+//        List<Studenten> studenten = studentenRepository.findByGruppeId(gruppe.getId());
+//        LocalDate monatStart = (monat != null) ? LocalDate.parse(monat + "-01") : LocalDate.now().withDayOfMonth(1);
+//        LocalDate monatEnde = monatStart.withDayOfMonth(monatStart.lengthOfMonth());
+//        List<Erfassung> erfassungen = erfassungRepository.findByStudenten_GruppeIdAndDatumBetween(gruppe.getId(), monatStart, monatEnde);
+//        model.addAttribute("gruppe", gruppe);
+//        model.addAttribute("studenten", studenten);
+//        model.addAttribute("erfassungen", erfassungen);
+//        model.addAttribute("monat", monatStart.toString().substring(0, 7)); // YYYY-MM
+//        model.addAttribute("tageImMonat", monatStart.lengthOfMonth());
+//        return "anwesenheitsliste";
+//    }
+//
+//
+//    @PostMapping("/liste/update/{id}")
+//    public String updateAnwesenheit(
+//            @PathVariable Integer id,
+//            @RequestParam Integer statusId,
+//            @RequestParam String kommentar) {
+//        Optional<Erfassung> optionalErfassung = erfassungRepository.findById(id);
+//        if (optionalErfassung.isPresent()) {
+//            Erfassung erfassung = optionalErfassung.get();
+//            Status neuerStatus = statusRepository.findById(statusId).orElse(null);
+//            if (neuerStatus != null) {
+//                erfassung.setStatus(neuerStatus);
+//                erfassung.setKommentar(kommentar);
+//                erfassungRepository.save(erfassung);
+//            }
+//        }
+//        return "redirect:/anwesenheitsliste/" + optionalErfassung.get().getStudenten().getGruppe().getId();
+//    }
 }
